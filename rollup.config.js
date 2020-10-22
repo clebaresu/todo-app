a103e22df8a5625c6a55fb40091293ac1e17683f
clebaresu <clebaresu@gmail.com>
2020-10-22 10:29:34 -0500

    Linea base Ok

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
 import merge from 'deepmerge';
+import copy from 'rollup-plugin-copy';
+
+
 // use createSpaConfig for bundling a Single Page App
 import { createSpaConfig } from '@open-wc/building-rollup';
 
@@ -7,14 +10,15 @@ import { createSpaConfig } from '@open-wc/building-rollup';
 
 const baseConfig = createSpaConfig({
   // use the outputdir option to modify where files are output
-  // outputDir: 'dist',
+  outputDir: 'dist',
 
   // if you need to support older browsers, such as IE11, set the legacyBuild
   // option to generate an additional build just for this browser
   // legacyBuild: true,
 
   // development mode creates a non-minified build for debugging or development
-  developmentMode: process.env.ROLLUP_WATCH === 'true',
+  // developmentMode: process.env.ROLLUP_WATCH === 'true',
+  
 
   // set to true to inject the service worker registration into your index.html
   injectServiceWorker: false,
@@ -24,8 +28,17 @@ export default merge(baseConfig, {
   // if you use createSpaConfig, you can use your index.html as entrypoint,
   // any <script type="module"> inside will be bundled by rollup
   input: './index.html',
-
   // alternatively, you can use your JS as entrypoint for rollup and
   // optionally set a HTML template manually
   // input: './app.js',
+  plugins: [
+    copy({
+      targets: [
+        { src: 'assets/**/*', dest: 'dist/assets' }
+      ],
+      // set flatten to false to preserve folder structure
+      flatten: false,
+    }),
+  ],
 });
+

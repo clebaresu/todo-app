62ab5334d6c1f552cc0218bb3015732384ba50d8
clebaresu <clebaresu@gmail.com>
2020-10-23 23:24:31 -0500

    en proceso typescript

--- src/views/todo-view.ts
+++ src/views/todo-view.ts
@@ -12,34 +12,23 @@ const VisibilityFilters = {     // (1)
 };
 
 class TodoApp extends LitElement {
-  todos: unknown[];
+  todos: never[];
   filter: string;
   task: string;
-  todo: string;
-  complete: boolean | undefined;
 
-
-  static get properties() {
-    return {
-      todos: { type: Array },
-      filter: { type: String },
-      task: { type: String }
-    };
-  }
   constructor() {
     super();
     this.todos = [];
     this.filter = VisibilityFilters.SHOW_ALL;
     this.task = '';
-    this.todo = '';
   }
   render() {
     return html`
       <div class="input-layout"
-      @keyup="${this.shortcutListener}">
+      @keyup="${this.shortcutListener()}">
 
       <vaadin-text-field
-        placeholder="Task"
+        placeholder="Tarea"
         value="${this.task}"
         @change="${this.updateTask}">
       </vaadin-text-field>
@@ -51,40 +40,11 @@ class TodoApp extends LitElement {
       </vaadin-button>
     </div>
 
-    <div class="todos-list">
-      ${this.todos.map(
-          todo => html`
-            <div class="todo-item">
-              <vaadin-checkbox
-                ?checked="${this.complete}"
-                @change="${ (e: { target: { checked: unknown } }) => this.updateTodoStatus(todo, e.target.checked)}">
-                ${this.task}
-              </vaadin-checkbox>
-            </div>
-          `
-        )
-      }
-    </div>
-
-    <div class="todos-list">
-      ${this.todos.map(
-          todo => html`
-            <div class="todo-item">
-              <vaadin-checkbox
-                ?checked="${this.complete}"
-                @change="${ (e: { target: { checked: unknown } }) => this.updateTodoStatus(todo, e.target.checked)}">
-                ${this.task}
-              </vaadin-checkbox>
-            </div>
-          `
-        )
-      }
-    </div>
-
     `;
   }
 
   addTodo() {
+    /*
     if (this.task) {
       this.todos = [...this.todos, {  // (1)
           task: this.task,
@@ -92,23 +52,29 @@ class TodoApp extends LitElement {
       }];
       this.task = '';   // (2)
     }
+    */
   }
 
-  shortcutListener(e: { key: string }) {
+  shortcutListener(e: {KeyboardEvent)} {
+    /*
     if (e.key === 'Enter') {      // (3)
       this.addTodo();
     }
+    */
   }
 
-  updateTask(e: { target: { value: string } }) {  //4)
+  updateTask(e) {  //4)
     this.task = e.target.value;
   }
 
-  updateTodoStatus(updatedTodo: unknown, complete: unknown) {
+   /*
+  updateTodoStatus(updatedTodo,complete) {
     this.todos = this.todos.map(todo =>
       updatedTodo === todo ? { ...updatedTodo, complete } : todo
     );
   }
+  */
+
 
 }
 
